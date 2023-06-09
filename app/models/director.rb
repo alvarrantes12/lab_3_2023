2885c09f5cf771fa505b93337dfb42b89867f45a
davidrorez <davidrorez@gmail.com>
2023-06-08 18:18:16 -0600

    Lab 3 API Done

---
+++ app/models/director.rb
@@ -0,0 +1,8 @@
+class Director < ApplicationRecord
+
+    has_many :movies
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"'
+  resources :directors
+
+  scope module: :api do
+    resources :movies
+  end
+
   root "dashboards#index"
 end
