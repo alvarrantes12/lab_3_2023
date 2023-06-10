23f7f0c811ce119e6f23ad31f8852ab846aa1aae
tyrsergio <sergio.hernandezjimenez@ucr.ac.cr>
2023-06-10 07:58:32 -0600

    SHJ-C13656 LabDone

---
+++ app/models/movie.rb
@@ -0,0 +1,5 @@
+class Movie < ApplicationRecord
+    belongs_to :director
+    validates :name, presence: true
+    validates :year, presence: true, length: {maximum: 4} ,numericality: { only_integer: true, less_than_or_equal_to: 2023, greater_than: 0}
+end
---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  subject {
+    described_class.new(name: "Pull Fuction",
+                        year: 2023,
+                        director: Director.create(first_name: "Jose", last_name: "Test1")
+                       )
+  }
+
+  it "valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without year" do
+    subject.year = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid if is not numeric" do
+    subject.year = "not a number"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a year more than 2023" do
+    subject.year = 2024
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a year with more than 4 numbers" do
+    subject.year = 20002
+    expect(subject).to_not be_valid
+  end
+end
