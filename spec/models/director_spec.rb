0bb0215c6f0b7086e50d56af19f87be508aa7897
The65Dude <marcobreda34@gmail.com>
2023-06-12 19:34:34 -0600

    Api Done

---
+++ spec/models/director_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Director, type: :model do
+  subject {
+    described_class.new(first_name: "Alejandro",
+                        last_name: "Aguilar"
+    )
+  }
+
+  it "valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without last name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without first name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+end
